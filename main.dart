13b0b4ea76cbbf4b50a281067a6587be0c471bb1
Somer0801 <61990861+Somer0801@users.noreply.github.com>
2022-11-04 17:35:48 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -18,7 +18,7 @@ void main() {
   List a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
   List evens = [];
   for (int i = 0; i < a.length; i++) {
-    if (a[i] % 2 != 0) {
+    if (a[i] % 2 == 0) {
       evens.add(a[i]);
     }
   }
